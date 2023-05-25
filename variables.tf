7305bf06cff27c52f6cf91995583b94d3559eada
Mårten Svantesson <marten.svantesson@ticket.se>
2023-05-25 18:38:19 +0200

    fix: don't install kuberhealthy by default
    kuberhealthy isn't working in current EKS versions

--- variables.tf
+++ variables.tf
@@ -64,5 +64,5 @@ variable "nginx_chart_version" {
 variable "install_kuberhealthy" {
   description = "Flag to specify if kuberhealthy operator should be installed"
   type        = bool
-  default     = true
+  default     = false
 }
