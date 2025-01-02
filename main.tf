b31d809f7cc191800970f0ff3e813c9be522971b
Mårten Svantesson <marten.svantesson@ticket.se>
2025-01-02 13:51:11 +0100

    fix: decode pem
    fixes #43

--- main.tf
+++ main.tf
@@ -120,7 +120,7 @@ resource "aws_eks_addon" "ebs-csi" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
@@ -133,7 +133,7 @@ provider "helm" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
